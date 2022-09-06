2ecf5e6039b53532149b51bea56ca46635d4311c
AbdelrahmanElrouby <90650851+AbdelrahmanElrouby@users.noreply.github.com>
2022-09-07 01:33:29 +0200

    Calculator added

---
+++ Basic_Calculator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double x , y ;
+    printf("Please enter the first number :") ; 
+    scanf("%lf",&x) ;
+    printf("Please enter the second number :") ; 
+    scanf("%lf",&y) ;
+    printf("The answer = %lf",x+y) ;
+    return 0;
+}
