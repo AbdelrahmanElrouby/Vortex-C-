f024b1a41e235f795fe3da7a87f37b7a2eb86489
AbdelrahmanElrouby <90650851+AbdelrahmanElrouby@users.noreply.github.com>
2022-09-16 07:44:21 +0200

    Update Mad_Libs.py

---
+++ Squad-asl_AbdelrahmanMohamed_Python/Mad_Libs.py
@@ -0,0 +1,4 @@
+Color = input("Please enter a color :")
+Plural_Noun = input("Please enter a plural noun :")
+Celebrity = input("Please enter a celebrity :")
+print("Rose are "+ Color +"\nViolets are " + Plural_Noun +"\nI love "+ Celebrity )
