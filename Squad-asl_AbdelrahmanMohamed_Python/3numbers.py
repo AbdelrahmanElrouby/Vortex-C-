b4061696c4392bbaab57ce221e69fed0f75f86b1
AbdelrahmanElrouby <90650851+AbdelrahmanElrouby@users.noreply.github.com>
2022-09-16 07:50:37 +0200

    Create 3numbers.py

---
+++ Squad-asl_AbdelrahmanMohamed_Python/3numbers.py
@@ -0,0 +1,6 @@
+Number1 = input("Please enter the first number :")
+Number2 = input("Please enter the second number :")
+Number3 = input("Please enter the third number :")
+Sum = float(Number1) + float(Number2) + float(Number3)
+print("The sum of the three numbers =" + str(Sum))
+print("The average of the three numbers =" + str(float(Sum/3.0)))
