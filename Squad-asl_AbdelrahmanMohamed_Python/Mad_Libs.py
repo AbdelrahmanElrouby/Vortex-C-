9f527eb1e613c77153ecde671e67d6c06f0767c8
AbdelrahmanElrouby <90650851+AbdelrahmanElrouby@users.noreply.github.com>
2022-09-19 17:08:56 +0200

    Comments added
    added comments of another print format as demanded

--- Squad-asl_AbdelrahmanMohamed_Python/Mad_Libs.py
+++ Squad-asl_AbdelrahmanMohamed_Python/Mad_Libs.py
@@ -2,3 +2,7 @@ Color = input("Please enter a color :")
 Plural_Noun = input("Please enter a plural noun :")
 Celebrity = input("Please enter a celebrity :")
 print("Rose are "+ Color +"\nViolets are " + Plural_Noun +"\nI love "+ Celebrity )
+# Another print format
+# data = dict( Color =  input("Please enter a color :") , Plural_Noun =  input("Please enter a plural noun :")  ,
+# Celebrity = input("Please enter a celebrity :") )
+# print("Rose are {Color} \nViolets are {Plural_Noun} \nI love {Celebrity}".format(**data) )
