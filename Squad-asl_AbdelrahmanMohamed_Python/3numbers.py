d23832eaf24e657f5cc4117448facd1b7ec0129f
AbdelrahmanElrouby <90650851+AbdelrahmanElrouby@users.noreply.github.com>
2022-09-19 16:59:32 +0200

    Comments added
    added comments with another solution using built-in functions as demanded

--- Squad-asl_AbdelrahmanMohamed_Python/3numbers.py
+++ Squad-asl_AbdelrahmanMohamed_Python/3numbers.py
@@ -1,6 +1,17 @@
+from math import *
 Number1 = input("Please enter the first number :")
 Number2 = input("Please enter the second number :")
 Number3 = input("Please enter the third number :")
 Sum = float(Number1) + float(Number2) + float(Number3)
-print("The sum of the three numbers =" + str(Sum))
-print("The average of the three numbers =" + str(float(Sum/3.0)))
+print("The sum of the three numbers = " + str(Sum))
+print("The average of the three numbers = " + str(float(Sum/3.0)))
+
+# Using built-in function
+# Number1 = int(input("Please enter the first number :") )
+# Number2 = int(input("Please enter the second number :") )
+# Number3 = int(input("Please enter the third number :") )
+# Arr=[Number1,Number2,Number3]
+# Sum = sum(Arr)
+# Average = Sum / len(Arr)
+# print("The sum of the three numbers = " + str(Sum))
+# print("The average of the three numbers = " + str(Average))
