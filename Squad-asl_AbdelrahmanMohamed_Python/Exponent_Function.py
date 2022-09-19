7b93eacd10d31e56d37d3930af6369e5d10b3e48
AbdelrahmanElrouby <90650851+AbdelrahmanElrouby@users.noreply.github.com>
2022-09-19 16:48:17 +0200

    comments added
    added two other ways using comments as demanded

--- Squad-asl_AbdelrahmanMohamed_Python/Exponent_Function.py
+++ Squad-asl_AbdelrahmanMohamed_Python/Exponent_Function.py
@@ -1,8 +1,17 @@
-def power(Number,power) :
-    result = 1 
-    for i in range(power) :
+def power(Number,power) :         
+    result = 1                    
+    for i in range(power) :       
         result *=Number
     return result
 n = input("Please enter the base number :")
 p = input("Please enter the power number :")
 print("The answer = " + str(power(int(n),int(p))))
+
+# Using built-in function  :
+# from math import *
+# ans = pow(int(n),int(p))
+# print( "The answer = " + str(ans))
+
+# Another solution :
+# ans = int(n)**int(p)
+# print( "The answer = " + str(ans))
